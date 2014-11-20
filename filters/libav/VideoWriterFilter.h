b251c77cc664b9b02cebf7d084cc42b1cbedd29a
Arash Shafiei <arash.shafiei@gmail.com>
2014-11-20 15:13:00 +0100

    video display modified for the new architecture

--- filters/libav/VideoWriterFilter.h
+++ filters/libav/VideoWriterFilter.h
@@ -1,8 +1,21 @@
 /*
- * VideoWriterFilter.h
+ * 
+ *  Tiny Multimedia Framework
+ *  Copyright (C) 2014 Arash Shafiei
  *
- *  Created on: Aug 18, 2014
- *      Author: arash
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef VIDEOWRITERFILTER_H_
@@ -34,29 +47,29 @@ public:
   
   virtual void init() {
     
+    Attribute * attr;
+    
     int width, height;
-    //int bitrate;
     
     string output_video = getProp("output_video");
     
-    /*
-     *  err = inMsg->getPropInt("width", width);
-     *  //if (err == MSG_NOT_FOUND)
-     *  //	return FILTER_WAIT_FOR_INPUT;
-     *  
-     *  err = inMsg->getPropInt("height", height);
-     *  //if (err == MSG_NOT_FOUND)
-     *  //	return FILTER_WAIT_FOR_INPUT;
-     */
-    
-    //bitrate = 400000;
+    inputPortRawFrame->lockAttr();
+    attr = inputPortRawFrame->getAttr();
+    width = stoi(attr->getProp("width"));
+    height = stoi(attr->getProp("height"));
+    inputPortRawFrame->unlockAttr();
     
     videoWriter->init(output_video, width, height);
-    
   }
   virtual void run() {
     
     inputPortRawFrame->lock();
+    
+    if (inputPortRawFrame->getStatus() == SampleStatus::EOS) {
+      status = FilterStatus::EOS; 
+      inputPortRawFrame->unlock();
+      return;
+    }
     
     RawFrame * inFrame = inputPortRawFrame->get();
     
--- tools/VideoDisplay.cpp
+++ tools/VideoDisplay.cpp
@@ -1,8 +1,21 @@
 /*
- * VideoDisplay.cpp
+ * 
+ *  Tiny Multimedia Framework
+ *  Copyright (C) 2014 Arash Shafiei
  *
- *  Created on: Aug 27, 2014
- *      Author: arash
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <tools/VideoDisplay.h>
