706860b33129bfe3ccf0bc55b96da5a681f51d4e
Arash Shafiei <arash.shafiei@gmail.com>
2015-10-17 17:17:09 +0000

    sensor and sender example

---
+++ filters/ehealth/EHealthSender.cpp
@@ -0,0 +1,47 @@
+/*
+ * 
+ *  Tiny Multimedia Framework
+ *  Copyright (C) 2014 Arash Shafiei
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "filters/ehealth/EHealthSender.h"
+
+using namespace tmf;
+using namespace std;
+using namespace ehealthsensor;
+
+FilterRegister<EHealthSender> EHealthSender::reg("ehealthsender");
+
+EHealthSender::EHealthSender(const string & name) :
+Filter(name) {
+  input = createInputPort<SensorData>("sensor input");
+}
+
+void EHealthSender::init() {
+
+}
+
+void EHealthSender::run() {
+  input->lock();
+  SensorData * inputData = input->get();
+  cout << inputData->temperature << endl;
+  input->unlock();   
+}
+
+EHealthSender::~EHealthSender() {
+  destroyPort(input);
+}
---
+++ filters/ehealth/EHealthSender.h
@@ -0,0 +1,49 @@
+/*
+ * 
+ *  Tiny Multimedia Framework
+ *  Copyright (C) 2014 Arash Shafiei
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef EHEALTHSENDER_H_
+#define EHEALTHSENDER_H_
+
+#include "core/tmf.h"
+#include "core/Filter.h"
+#include "core/Port.h"
+
+#include <ehealthsensor/Sensor.h>
+
+struct EHealthSender: public tmf::Filter {
+private:
+
+  tmf::InputPort<ehealthsensor::SensorData> * input;
+
+  static tmf::FilterRegister<EHealthSender> reg;
+    
+public:
+  
+  EHealthSender(const std::string& name);
+  
+  virtual void init();
+  
+  virtual void run(); 
+  
+  virtual ~EHealthSender();
+  
+};
+
+#endif /* EHEALTHSENDER_H_ */
