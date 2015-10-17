a285614034fcfa861927d106d6c14900878a9668
Arash Shafiei <arash.shafiei@gmail.com>
2015-10-17 19:31:37 +0330

    ehealth sensor

---
+++ filters/ehealth/EHealthSensor.h
@@ -0,0 +1,51 @@
+/*
+ * 
+ *  Tiny Multimedia Framework
+ *  Copyright (C) 2014 Arash Shafiei
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef EHEALTHSENSOR_H_
+#define EHEALTHSENSOR_H_
+
+#include "core/tmf.h"
+#include "core/Filter.h"
+#include "core/Port.h"
+
+#include "filters/ehealth/tools/EHealthData.h"
+
+
+struct EHealthSensor: public tmf::Filter {
+private:
+
+  tmf::OutputPort<int> * output;
+
+  static tmf::FilterRegister<EHealthSensor> reg;
+  
+
+public:
+  
+  EHealthSensor(const std::string& name);
+  
+  virtual void init();
+  
+  virtual void run(); 
+  
+  virtual ~EHealthSensor();
+  
+};
+
+#endif /* AIRFLOWSENSOR_H_ */
