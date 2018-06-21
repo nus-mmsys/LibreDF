58a65d71b2c4f6d89c8e0c47f7871276b384b21f
rshfsr <rshfsr@gmx.com>
2018-06-21 13:39:38 +0200

    Update graph.h

--- src/shell/graph.h
+++ src/shell/graph.h
@@ -16,8 +16,8 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef GRAPH_H
-#define GRAPH_H
+#ifndef TDF_GRAPH_H
+#define TDF_GRAPH_H
 
 #include <algorithm>
 #include <iostream>
