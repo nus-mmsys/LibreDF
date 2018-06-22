7487c72c322b3413f549fbd2d8a563267a8ad3cd
Arash Shafiei <arash.shafiei@inria.fr>
2018-06-22 13:10:46 +0200

    pool actor.

---
+++ src/dataflow/actors/machine-learning/pool.h
@@ -0,0 +1,53 @@
+/*
+ *   DF
+ *   Copyright (C) 2018
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DF_POOL_H_
+#define DF_POOL_H_
+
+#include "core/df.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Pool: public df::Actor {
+  
+private:
+
+  cv::Mat frame;
+  double scalex, scaley;
+  df::InputPort<cv::Mat> * input;
+  df::OutputPort<cv::Mat> * output;
+ 
+  static  df::ActorRegister<Pool> reg;
+public:
+  
+  Pool(const string& name);
+  
+  virtual void init();
+  
+  virtual void run();
+  
+  virtual ~Pool();
+  
+};
+
+#endif /* DF_POOL_H_ */
