4f47744246f3b86e08d926882bd68c5bbe0c2438
rshfsr <rshfsr@gmx>
2018-06-27 19:05:53 +0200

    opencv message

--- build/build.sh
+++ build/build.sh
@@ -1,15 +1,23 @@
 #! /usr/bin/env bash
 
+OS=`uname`
 OCV=`pkg-config --cflags opencv`
-if [ "$OCV" == "" ]
-then
-	echo "opencv is not installed."
-	exit 1
-fi
-
 OCV=`echo "$OCV" | cut -d " " -f 1`
 OCV=${OCV#-I}
 OCV=${OCV%include/opencv}
+
+if [ ${OCV:0:1} != "/" ]
+then
+	echo "opencv is not installed."
+	if [ $OS == "Linux" ]
+	then
+		echo "apt install libopencv-dev"
+	elif [ $OS == "Darwin" ]
+	then
+		echo "brew install opencv"
+	fi
+	exit 1
+fi
 
 DF_HOME="${HOME}/Documents/df"
 if [ ! -d $DF_HOME ]
@@ -17,14 +25,14 @@ then
 	mkdir $DF_HOME
 fi
 CASCADE="${DF_HOME}/haarcascades"
-if [ ! -d ${CASCADE} ]
+if [ ! -d $CASCADE ]
 then
-	mkdir ${CASCADE}
+	mkdir $CASCADE
 fi
 OUTPUTS="${DF_HOME}/outputs"
-if [ ! -d ${OUTPUTS} ]
+if [ ! -d $OUTPUTS ]
 then
-	mkdir ${OUTPUTS}
+	mkdir $OUTPUTS
 fi
 cp -n ${OCV}/share/opencv/haarcascades/*.xml ${CASCADE}/
 if [ $# == 0 ]
