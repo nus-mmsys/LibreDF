571b4fd84433a09605c6cf1ce6159f59c41a2dd9
Arash Shafiei <arash.shafiei@inria.fr>
2018-07-10 13:00:51 +0200

    remote actor skeleton

---
+++ src/dataflow/actors/basic/remote.h
@@ -0,0 +1,38 @@
+/*
+ *   libdataflow
+ *   Copyright (C) 2018
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DF_REMOTE_H_
+#define DF_REMOTE_H_
+
+#include "core/df.h"
+
+#include <iostream>
+
+class Remote: public df::Actor {
+  
+private:
+  static df::ActorRegister<Remote> reg;
+public:
+  
+  Remote(const std::string & name): df::Actor(name) {}
+  virtual void run() {}
+  virtual ~Remote() {}
+  
+};
+
+#endif /* DF_REMOTE_H_ */
