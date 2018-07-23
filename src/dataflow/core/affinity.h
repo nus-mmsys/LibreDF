aff3009912ce253f41e6dd5f6afdb5370d1be310
Arash Shafiei <arash.shafiei@inria.fr>
2018-07-23 16:22:24 +0200

    refactor

--- src/dataflow/core/affinity.h
+++ src/dataflow/core/affinity.h
@@ -80,16 +80,14 @@
   }
  
   static int pthread_setaffinity_np(pthread_t thread, size_t cpu_size, cpu_set_t *cpu_set) {
-	thread_port_t mach_thread;
+	thread_port_t mach_thread = pthread_mach_thread_np(thread);
 	int core = 0;
 
 	for (core = 0 ; core < 8 * cpu_size ; core++) {
 		if (CPU_ISSET(core, cpu_set))
 			break;
 	}
-
-	thread_affinity_policy_data_t policy = { core };
-	mach_thread = pthread_mach_thread_np(thread);
+	thread_affinity_policy policy = { core };
 	thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY, 
 			(thread_policy_t)&policy, 1);
 	return 0;
