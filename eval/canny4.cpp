197b9ef9195c8493a0667db922caa3894215574f
rshfsr <rshfsr@gmx.com>
2018-08-20 09:32:38 +0200

    parallel canny

--- eval/canny4.cpp
+++ eval/canny4.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -7,19 +10,33 @@
 using namespace std;
 using namespace cv;
 
+struct PMat {
+	Mat mat;
+	condition_variable cons;
+	condition_variable prod;
+	mutex mux;
+};
+
+void canny_detection(PMat * pmat) {
+
+	cout << "canny\n";
+}
+
 int main(int argc, char ** argv) {
 
 	string file_name = "pedestrian.mp4";
 	if (argc >= 2) {
 		file_name = argv[1];
 	}
-
 	Mat frame, gray, canny;
 	int cvt = CV_BGR2GRAY;
 	int threshold = 100;
 	int ratio = 2;
 	int kernel_size = 3;
 	int stepno = 0;
+	int level = 2;
+	PMat * out[level];
+	thread * th[level];
 	clock_t start, end;
 	string home_path = getenv("HOME");
   	string dfout_path = home_path + "/Documents/df/outputs/";
@@ -27,6 +44,11 @@ int main(int argc, char ** argv) {
 	string file_out;
 
 	start = clock();
+
+	for (int i=0; i<level*level; i++) {
+		out[i] = new PMat();
+		th[i] = new thread(&canny_detection, out[i]);
+	}
 
 	VideoCapture * cap = new VideoCapture(file_in);
 
@@ -36,8 +58,20 @@ int main(int argc, char ** argv) {
 
 	*cap >> frame;
 	
+	int tilew = frame.cols / level;
+        int tileh = frame.rows / level;
+
 	while (!frame.empty()) {
-		
+	
+		for (int j=0; j<level ; j++) {
+			for (int i=0; i<level ; i++) {
+				Rect tile(i*tilew, j*tileh, tilew, tileh);
+				//out[j*level+i].mux.lock();
+				//out[j*level+i].mat = (frame(tile)).clone();
+				//out[j*level+i].mux.unlock();
+			}
+		}
+							
 		cv::cvtColor(frame, gray, cvt);
 		cv::blur(gray, gray, Size(3,3));  
 		cv::Canny(gray, canny, threshold, threshold*ratio, kernel_size);
@@ -46,6 +80,10 @@ int main(int argc, char ** argv) {
 		*cap >> frame;
 		cout << "[" << stepno << "]" << " processing frame " << "\n";
 		stepno++;
+	}
+
+	for (int i=0; i<level*level; i++) {
+		th[i]->join();
 	}
 
 	end = clock();
