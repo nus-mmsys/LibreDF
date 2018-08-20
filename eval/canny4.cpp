b89dc697a5b36d87ed7aa55c015afb5f7ea6967f
rshfsr <rshfsr@shafiei.ddns.net>
2018-08-21 00:20:31 +0200

    canny example

--- eval/canny4.cpp
+++ eval/canny4.cpp
@@ -18,8 +18,6 @@ private:
 	bool consumed;
 public:
 	Mat mat;
-	Mat gray;
-	Mat canny;
 	bool last;
 
 	PMat() {
@@ -48,24 +46,61 @@ public:
 	}
 };
 
-void canny_detection(PMat * pmat) {
+string home_path = getenv("HOME");
+string dfout_path = home_path + "/Documents/df/outputs/";
+
+int level;
+int tilew;
+int tileh;
+int type;
+PMat ** pmat;
+PMat ** pcanny;
+
+void canny_detection(int i) {
 	bool finished = false;
 	int cvt = CV_BGR2GRAY;
 	int thrshold = 100;
 	int rtio = 2;
 	int kernel_size = 3;
-
+	Mat gray;
 	while(!finished) {
-		pmat->consumerLock();
-		if (!pmat->last) {
-			cv::cvtColor(pmat->mat, pmat->gray, cvt);
-			cv::blur(pmat->gray, pmat->gray, Size(3,3));  
-			cv::Canny(pmat->gray, pmat->canny, thrshold, thrshold*rtio, kernel_size);
+		pmat[i]->consumerLock();
+		if (!pmat[i]->last) {
+			cv::cvtColor(pmat[i]->mat, gray, cvt);
+			cv::blur(gray, gray, Size(3,3));
+			pcanny[i]->producerLock();  
+			cv::Canny(gray, pcanny[i]->mat, thrshold, thrshold*rtio, kernel_size);
+			pcanny[i]->producerUnlock();
 		}
 
-		finished = pmat->last;
-		pmat->consumerUnlock();
-	}	
+		finished = pmat[i]->last;
+		pmat[i]->consumerUnlock();
+	}
+	pcanny[i]->producerLock();
+	pcanny[i]->last = true;
+	pcanny[i]->producerUnlock();	
+}
+
+void canny_merge() {
+	Mat fout = Mat(tileh*level, tilew*level, CV_32FC1);
+	bool finished;
+	int stepno = 0;
+	string file_out;
+	while(!finished) {
+		for (int j=0; j<level ; j++) {
+			for (int i=0; i<level ; i++) {
+				Rect tile(i*tilew, j*tileh, tilew, tileh);
+				int idx = j*level+i;
+				pcanny[idx]->consumerLock();	
+				finished = pcanny[idx]->last;
+				pcanny[idx]->mat.copyTo(fout(tile));
+				pcanny[idx]->consumerUnlock();
+			}
+		}
+		file_out = dfout_path + to_string(stepno) + ".png";
+		cv::imwrite(file_out, fout); 
+		stepno++;
+	}
 }
 
 int main(int argc, char ** argv) {
@@ -75,22 +110,17 @@ int main(int argc, char ** argv) {
 		file_name = argv[1];
 	}
 	Mat frame;
+	Mat fout;
 	int stepno = 0;
-	int level = 2;
-	PMat * out[level];
+	level = 2;
+	pmat = new PMat*[level];
+	pcanny = new PMat*[level];
 	thread * th[level];
+	thread * thmerge;
 	clock_t start, end;
-	string home_path = getenv("HOME");
-  	string dfout_path = home_path + "/Documents/df/outputs/";
 	string file_in =  home_path + "/Documents/df/" + file_name;
-	string file_out;
 
 	start = clock();
-
-	for (int i=0; i<level*level; i++) {
-		out[i] = new PMat();
-		th[i] = new thread(&canny_detection, out[i]);
-	}
 
 	VideoCapture * cap = new VideoCapture(file_in);
 
@@ -100,9 +130,18 @@ int main(int argc, char ** argv) {
 
 	*cap >> frame;
 	
-	int tilew = frame.cols / level;
-        int tileh = frame.rows / level;
+	tilew = frame.cols / level;
+        tileh = frame.rows / level;
+	type = frame.type();
+	
+	for (int i=0; i<level*level; i++) {
+		pmat[i] = new PMat();
+		pcanny[i] = new PMat();
+		th[i] = new thread(&canny_detection, i);
+	}
 
+	thmerge = new thread(&canny_merge);
+	
 	while (!frame.empty()) {
 	
 		for (int j=0; j<level ; j++) {
@@ -110,29 +149,27 @@ int main(int argc, char ** argv) {
 				Rect tile(i*tilew, j*tileh, tilew, tileh);
 				int idx = j*level+i;
 			
-				out[idx]->producerLock();	
-				out[idx]->mat = (frame(tile)).clone();
-				cout << "channels " << out[idx]->mat.channels() << "\n";
-				out[idx]->producerUnlock();
+				pmat[idx]->producerLock();	
+				pmat[idx]->mat = (frame(tile)).clone();
+				pmat[idx]->producerUnlock();
 			}
 		}
-		file_out = dfout_path + to_string(stepno) + ".png";
-		//cv::imwrite(file_out, canny); 
+
 		*cap >> frame;
 		cout << "[" << stepno << "]" << " processing frame " << "\n";
 		stepno++;
 	}
 
 	for (int i=0; i<level*level ; i++) {
-		out[i]->producerLock();
-		out[i]->last = true;
-		out[i]->producerUnlock();
+		pmat[i]->producerLock();
+		pmat[i]->last = true;
+		pmat[i]->producerUnlock();
 	}
 		
 	for (int i=0; i<level*level; i++) {
 		th[i]->join();
 	}
-
+	thmerge->join();
 	end = clock();
 	cout << "Execution time = "+to_string(double(end - start)/CLOCKS_PER_SEC) << "\n"; 
 	return 0;
