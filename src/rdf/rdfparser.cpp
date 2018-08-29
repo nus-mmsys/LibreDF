fe7982de1f38f466d6ce8de7d8f75fce92161ecf
rshfsr <rshfsr@gmx.com>
2018-08-29 13:50:34 +0200

    comment

--- src/rdf/rdfparser.cpp
+++ src/rdf/rdfparser.cpp
@@ -1,6 +1,6 @@
 /*
  *   RDF
- *   Copyright (C) 2018
+ *   Copyright (C) INRIA - Orange Labs
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
