867b11647d3d29d2bf354a84f6785e5a8398fafa
rshfsr <rshfsr@gmx.com>
2018-08-30 19:38:18 +0200

    rename

--- build/eval-build.sh
+++ build/eval-build.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 ../eval/canny-split4.cpp -o canny-split4 -pthread `pkg-config --cflags --libs opencv`
+g++ -std=c++11 ../eval/canny-parallel4.cpp -o canny-parallel4 -pthread `pkg-config --cflags --libs opencv`
