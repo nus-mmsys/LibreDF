eabc8dafe8df365e7e4c6456fbf21a70d26a1086
rshfsr <rshfsr@gmx.com>
2018-09-08 16:09:58 +0200

    hamilton2

---
+++ src/dataflow/actors/combinatorics/hamilton2.cpp
@@ -0,0 +1,84 @@
+/*
+ *   TMF
+ *   Copyright (C) TMF Team
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "hamilton2.h"
+
+using namespace df;
+using namespace std;
+
+ActorRegister<Hamilton2> Hamilton2::reg("Hamilton2");
+
+Hamilton2::Hamilton2(const string & name) : Actor(name) {
+  input = createInputPortVector<Str>("input");
+  output = createOutputPortVector<Str>("output");
+}
+
+void Hamilton2::init() {
+  if (!propEmpty("nbnodes")) {
+    nbnodes = getPropInt("nbnodes");
+  } else
+    nbnodes = 0;
+  first = true;  
+}
+void Hamilton2::run() {
+
+  if (first) {
+	auto out = produce(output);
+  	for (auto o : out) {
+  		o->set(name);
+  	}
+  	release(output);
+	first = false;
+	return;
+  }
+
+  input_messages = "";
+  auto in = consume(input);
+  for (auto i : in) {
+	input_messages = *i->get() + ";" + input_messages;
+  }
+  release(input);
+
+  std::stringstream ss(input_messages);
+
+  output_message = "";
+  while (std::getline(ss, msg, ';')) {
+  	  
+	if (msg!="" && msg.find(name)==string::npos) {
+    		
+		msg = msg + "," + name;
+
+	  	if (std::count(msg.begin(), msg.end(), ',') == nbnodes-1) {
+    			log("Hamiltonian path: "+msg);
+		} else {
+			output_message = msg + ";" + output_message;
+		}		
+  	}
+  }
+
+  auto out = produce(output);
+  for (auto o : out) {
+	o->set(output_message);
+  }
+  release(output);
+}
+
+Hamilton2::~Hamilton2() {
+  destroyPort(input);
+  destroyPort(output);
+}
---
+++ src/dataflow/actors/combinatorics/hamilton2.h
@@ -0,0 +1,51 @@
+/*
+ *   TMF
+ *   Copyright (C) TMF Team
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DF_HAMILTON2_H_
+#define DF_HAMILTON2_H_
+
+#include "core/df.h"
+#include "tokens/basic/str.h"
+
+#include <string>
+#include <iostream>
+
+class Hamilton2: public df::Actor {
+  
+private:
+  df::InputPortVector<df::Str> * input;
+  df::OutputPortVector<df::Str> * output;
+
+  std::string msg;
+  std::string input_messages;
+  std::string output_message;
+  int nbnodes;
+  bool first;
+  static df::ActorRegister<Hamilton2> reg;
+public:
+  
+  Hamilton2(const std::string & name);
+  
+  virtual void init();
+  virtual void run();
+  
+  virtual ~Hamilton2();
+  
+};
+
+#endif /* DF_HAMILTON2_H_ */
