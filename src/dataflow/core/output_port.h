1febb0154bcbf835467f4c30e576c10ad0d7ef97
rshfsr <rshfsr@gmx.com>
2019-04-10 11:04:44 +0200

    buffer size increased

--- src/dataflow/core/output_port.h
+++ src/dataflow/core/output_port.h
@@ -57,7 +57,7 @@ namespace df {
      */
     OutputPort<T>(std::string name) : OPort(name), index(0) {
       sock = new ClientSocket("port:"+name);
-      buf = new Buffer<T>(16);
+      buf = new Buffer<T>(32);
       port_cap = std::string(typeid(T).name());
       data = new T();
       for (int i=0; i<rate; i++)
