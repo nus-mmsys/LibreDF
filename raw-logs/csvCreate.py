8a4fd442e09a06760137652f0ebda91035e9ec83
rshfsr <rshfsr@gmx.com>
2019-05-28 16:55:17 +0200

    csvCreate and results

--- raw-logs/csvCreate.py
+++ raw-logs/csvCreate.py
@@ -7,5 +7,26 @@ if len(sys.argv) < 2 :
     exit(0)
 
 f = open(sys.argv[1])
+res = open("results.csv", "w")
+
+csv = {}
+
+for line in f:
+    linelist = line.split()
+    it = int(linelist[2])
+    start = int(linelist[4])
+    end = int(linelist[6])
+    if it in csv :
+        if start < csv[it][0] :
+            csv[it][0] = start
+        if end > csv[it][1] :
+            csv[it][1] = end
+    else:
+        csv[it] = [start, end]
+
+res.write("iteration,start,end\n")
+
+for it in dict(sorted(csv.items())):
+    res.write(str(it)+","+str(csv[it][0])+","+str(csv[it][1])+"\n")
 
 f.close()
