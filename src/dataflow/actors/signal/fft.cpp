bd4795d3ad8d9af426819f4b7770c469180b9b49
rshfsr <rshfsr@gmx.com>
2019-06-03 12:23:28 +0200

    fix size

--- src/dataflow/actors/signal/fft.cpp
+++ src/dataflow/actors/signal/fft.cpp
@@ -45,7 +45,7 @@ void FFT::run() {
   const int m = in->vector_size()/2;
   const double theta0 = 2*M_PI/in->vector_size();
 
-  for (int p = 0; p < in->vector_size(); p++) {
+  for (int p = 0; p < m; p++) {
             const std::complex<double> wp = cos(p*theta0)+i*(-sin(p*theta0));
             const std::complex<double> a = in->get_item(p + 0);
             const std::complex<double> b = in->get_item(p + m);
