1d6fbd044ec7b14b8041235a4c4cb556827209f1
rshfsr <rshfsr@gmx.com>
2019-06-03 10:50:52 +0200

    complex producer/consumer

---
+++ src/dataflow/actors/signal/complex_producer.h
@@ -0,0 +1,54 @@
+/*
+ *   TMF
+ *   Copyright (C) TMF Team
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DF_COMPLEXPRODUCER_H_
+#define DF_COMPLEXPRODUCER_H_
+
+#include "core/df.h"
+#include "tokens/basic/complex_vector.h"
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+using namespace std;
+
+class ComplexProducer: public df::Actor {
+  
+private:
+
+  int step;
+  int number;  
+  int last;
+  
+  df::OutputPort<df::ComplexVector> * output;
+ 
+  static  df::ActorRegister<ComplexProducer> reg;
+public:
+  
+  ComplexProducer(const string& name);
+  
+  virtual void init();
+  
+  virtual void run();
+  
+  virtual ~ComplexProducer();
+  
+};
+
+#endif /* DF_COMPLEXPRODUCER_H_ */
