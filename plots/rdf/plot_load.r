c17d6e86b879d23744f69a1be58fc1518c3178e9
rshfsr <rshfsr@gmx.com>
2019-08-16 16:21:49 +0200

    plot

--- plots/rdf/plot_load.r
+++ plots/rdf/plot_load.r
@@ -17,12 +17,20 @@ name <- unlist(strsplit(args[1],"[.]"))[1]
 
 pdf(paste(name,".pdf", sep=""))
 
+ggtheme = theme(plot.title = element_text(size=15),
+	axis.title.x = element_text(size=14),
+	axis.text.x = element_text(size=12),
+	axis.title.y = element_text(size=14),
+	axis.text.y = element_text(size=12),
+	legend.title = element_text(size=14),
+	legend.text = element_text(size=12))
+
 scale = 1000000
 
 ggplot(data, aes(data$iteration, data$exectime/scale, color = data$actor)) +
-    geom_point() +
     geom_path(aes(group = data$actor)) +
-    labs(title = name, x = "iteration", y = "latency [second]", color = "actor")
+    labs(x = "iteration", y = "latency [second]", color = "actor") +
+    ggtheme
 
 #plot(data$iteration, data$exectime/scale, 
 #     main=name, 
